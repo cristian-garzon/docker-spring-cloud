e1aa530684e97b587c7bb887a006d2d8927da83f
Cristian Camilo Garzon Rodriguez <cristiancamilogarzon2@gmail.com>
2021-09-24 23:14:39 -0500

    adding gateway zuul

---
+++ microservice-zuul/src/main/java/com/springdocker/app/zuul/MicroserviceZuulApplication.java
@@ -0,0 +1,17 @@
+package com.springdocker.app.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class MicroserviceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceZuulApplication.class, args);
+    }
+
+}
