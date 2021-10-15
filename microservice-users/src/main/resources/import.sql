ce07f71de508be70e439bfb0c4130741c513f252
Cristian Camilo Garzon Rodriguez <cristiancamilogarzon2@gmail.com>
2021-10-14 23:14:33 -0500

    add jwt token and basic auth

--- microservice-users/src/main/resources/import.sql
+++ microservice-users/src/main/resources/import.sql
@@ -1,7 +1,7 @@
-insert INTO users (enabled, password, user, name, surname, email) VALUES (true ,'cristian123','cristian1','cristian','garzon','cristiancamilogarzon2@gmail.com');
-insert INTO users (enabled, password, user, name, surname, email) VALUES (false,'camilo123','camilo1','camilo','rodriguez','camilo@gmail.com');
-insert INTO users (enabled, password, user, name, surname, email) VALUES (false,'cristian12345','cristian','garzon','cristian','cristiancamilogarzon@ucundinamarca.edu.co');
-insert INTO users (enabled, password, user, name, surname, email) VALUES (true,'camilo12345','camilo2','rodriguez','camilo','camilo@ucundinamarca.edu.co');
+insert INTO users (enabled, password, user, name, surname, email) VALUES (true ,'$2a$10$LiXYS2XQH6GP.Z8eeSzZFumqpev.zStDxJ9W1MKptOVB1rPqPphoq','cristian1','cristian','garzon','cristiancamilogarzon2@gmail.com');
+insert INTO users (enabled, password, user, name, surname, email) VALUES (false,'$2a$10$Z5Cljz.0nNA0DYm1vg9NK.mlxvLyHPT9QExiysrEkckiYV1qq0PjK','camilo1','camilo','rodriguez','camilo@gmail.com');
+insert INTO users (enabled, password, user, name, surname, email) VALUES (false,'$2a$10$FKoL55KlGojAGhUJkaoXV.E5.xifLyVITSd4O7YXUmvMyrecFaGrG','cristian','garzon','cristian','cristiancamilogarzon@ucundinamarca.edu.co');
+insert INTO users (enabled, password, user, name, surname, email) VALUES (true,'$2a$10$Am2dYTxcsEwK6njvDdJC/eM7aH3McNnsyQ/DBdD.OPuqiwwhvlmyq','camilo2','rodriguez','camilo','camilo@ucundinamarca.edu.co');
 
 insert INTO roles (name) VALUES ('ROLE_MOD');
 insert INTO roles (name) VALUES ('ROLE_ADMIN');
