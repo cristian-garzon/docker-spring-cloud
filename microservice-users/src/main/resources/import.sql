85918f39a6766ad13509b708322444d5da8a1810
cristian-garzon <cristiancamilogarzon2@gmail.com>
2021-10-17 19:05:52 -0500

    validate routes and permit roles

--- microservice-users/src/main/resources/import.sql
+++ microservice-users/src/main/resources/import.sql
@@ -1,5 +1,5 @@
 insert INTO users (enabled, password, username, name, surname, email) VALUES (true ,'$2a$10$VmqRkHaNxCt179fPyfTvQ.1UdWvD/my5WGp2wBHDrGW4yvSuL2l.K','cristian1','cristian','garzon','cristiancamilogarzon2@gmail.com');
-insert INTO users (enabled, password, username, name, surname, email) VALUES (false,'$2a$10$U3ilRDFh4t4JrrpABxc6W..qbogGp7NUbKgW5KS7OtIMvdk5q/qci','camilo1','camilo','rodriguez','camilo@gmail.com');
+insert INTO users (enabled, password, username, name, surname, email) VALUES (true,'$2a$10$U3ilRDFh4t4JrrpABxc6W..qbogGp7NUbKgW5KS7OtIMvdk5q/qci','camilo1','camilo','rodriguez','camilo@gmail.com');
 insert INTO users (enabled, password, username, name, surname, email) VALUES (false,'$2a$10$bBqIY9fPVVi4YeuYSgJW3eKyAS3joYtsGyI1E0xGFsjYz/9nxoRF6','cristian','garzon','cristian','cristiancamilogarzon@ucundinamarca.edu.co');
 insert INTO users (enabled, password, username, name, surname, email) VALUES (true,'$2a$10$4L2/kqt2mCEUlviXMmj7res2xEeGRVbf8PT8KdhKJS0iamLjvqrA6','camilo2','rodriguez','camilo','camilo@ucundinamarca.edu.co');
 
---
+++ microservice-zuul/src/main/java/com/springdocker/app/zuul/oauth/ResourceServerConfig.java
@@ -0,0 +1,45 @@
+package com.springdocker.app.zuul.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+
+    @Override
+    // for the token
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore());
+    }
+
+    @Override
+    //for the routes
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/Oauth/oauth/token").permitAll().
+        antMatchers(HttpMethod.GET, "/api/Product/list","/api/Item/list", "/api/User/user").permitAll().
+        antMatchers(HttpMethod.GET, "/api/find/{id}", "/api/Item/find/size/{size}", "/api/User/user/{id}").
+        hasAnyRole("ADMIN", "MOD").
+        antMatchers( "/api/Product/**", "/api/Item/**", "/api/User/**").hasRole("ADMIN").
+        anyRequest().authenticated();
+    }
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter token = new JwtAccessTokenConverter();
+        token.setSigningKey("secret_key");
+        return token;
+    }
+}
